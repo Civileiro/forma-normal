3151a1086c819bc9d41b45b66e6c450738e1a5e8
Civi <gabrielprost@hotmail.com>
2022-03-29 04:09:28 -0300

    changed app window name

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int, char**)
 #endif
 
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Calculadora de Forma Normal", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
